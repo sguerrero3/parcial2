80b0a94a0f10d7b4a02729e390089b4badfb67c1
sguerrero3 <s.guerrero3@uniandes.edu.co>
2024-05-16 10:19:10 -0500

    Configurar base de datos postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'proyecto',
+    entities: [/*Incluir Entidades*/],
+    dropSchema: true,
+    synchronize: true,
+    keepConnectionAlive: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
